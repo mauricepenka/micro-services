76f233fb4bdcd5f3485580916c67223c5c566152
maurice.penka <penka-saadeu.m@sfeir.com>
2019-05-29 15:55:00 +0200

    create components humidity and pressure

--- weatherApp/src/app/app.module.ts
+++ weatherApp/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TemperatureComponent } from './components/temperature/temperature.component';
 import { LocationComponent } from './components/location/location.component';
+import { HumidityComponent } from './components/humidity/humidity.component';
+import { AtmosphericPressureComponent } from './components/atmospheric-pressure/atmospheric-pressure.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TemperatureComponent,
-    LocationComponent
+    LocationComponent,
+    HumidityComponent,
+    AtmosphericPressureComponent
   ],
   imports: [
     BrowserModule,
---
+++ weatherApp/src/app/components/atmospheric-pressure/atmospheric-pressure.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-atmospheric-pressure',
+  templateUrl: './atmospheric-pressure.component.html',
+  styleUrls: ['./atmospheric-pressure.component.css']
+})
+export class AtmosphericPressureComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
